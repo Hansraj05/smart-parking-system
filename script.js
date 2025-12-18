1420606d3123a53849708f099a1d45a4629dc848
Hansraj Bharadwaj <hansrajbharadwaj508@gmail.com>
2025-12-18 13:39:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -112,7 +112,7 @@ function checkAutomaticStatus(uLat, uLng) {
  */
 async function fetchParking(lat, lng) {
     try {
-        const res = await fetch('https://smart-parking-system-4.onrender.com/predict', {
+        const res = await fetch('http://127.0.0.1:5000/predict', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({ latitude: lat, longitude: lng })
@@ -155,7 +155,7 @@ async function fetchParking(lat, lng) {
  * UTILS: API Reporting & UI Notifications
  */
 async function reportActivity(name, action) {
-    await fetch('https://smart-parking-system-4.onrender.com/update_activity', {
+    await fetch('http://127.0.0.1:5000/update_activity', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({ name: name, action: action })
